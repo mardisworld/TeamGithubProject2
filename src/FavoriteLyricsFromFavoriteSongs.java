35ebc8107852a28fd2d9e4800f22886a0f5373b6
ggurevich <gurevichgabriel@gmail.com>
2016-11-28 15:41:05 -0500

    changed lyric

--- src/FavoriteLyricsFromFavoriteSongs.java
+++ src/FavoriteLyricsFromFavoriteSongs.java
@@ -4,7 +4,7 @@ public class FavoriteLyricsFromFavoriteSongs {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("I am just like my country, I'm young, scrappy, and hungry, and I am not throwing away my shot.");
+		System.out.println("come be wild with me");
 
 	}
 
