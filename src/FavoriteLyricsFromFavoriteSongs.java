00f0a273fbffae01f8a20aab3db9c68275cd78b4
Marissa Staller <mardisworld@gmail.com>
2016-11-28 15:24:00 -0500

    Initial commit

---
+++ src/FavoriteLyricsFromFavoriteSongs.java
@@ -0,0 +1,11 @@
+
+public class FavoriteLyricsFromFavoriteSongs {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("I am just like my country, I'm young, scrappy, and hungry, and I am not throwing away my shot.");
+
+	}
+
+}
